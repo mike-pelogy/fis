dad03814b9bd7c7523d47d016035076c8ed72b43
Erik Freundorfer <e.freundorfer@gmail.com>
2025-02-01 16:44:40 -0500

    form fix

--- src/utils/submitForm.ts
+++ src/utils/submitForm.ts
@@ -1,24 +1,32 @@
 import { FormEvent } from "react";
 import { toast } from "react-toastify";
 
-export const handleSubmit = (isForm = false) => async (event: FormEvent) => {
-  event.preventDefault();
+export const handleSubmit =
+  (isForm = false) =>
+  async (event: FormEvent) => {
+    event.preventDefault();
 
-  const myForm = event.target as HTMLFormElement;
-  // eslint-disable-next-line
-  const formData: any = new FormData(myForm);
+    const myForm = event.target as HTMLFormElement;
+    // eslint-disable-next-line
+    const formData: any = new FormData(myForm);
 
-  await fetch("/", {
-    method: "POST",
-    headers: !isForm ? { "Content-Type": "application/x-www-form-urlencoded" } : {},
-    body: new URLSearchParams(formData).toString(),
-  })
-    .then(() => {
-      toast.success("Form submitted successfully");
-      myForm.reset();
+    await fetch("/", {
+      method: "POST",
+      headers: !isForm
+        ? { "Content-Type": "application/x-www-form-urlencoded" }
+        : {},
+      body: new URLSearchParams(formData).toString(),
     })
-    .catch((e) => {
-      console.error(e);
-      toast.error("Form submission failed");
-    });
-};
+      .then(() => {
+        toast.success("Form submitted successfully");
+        myForm.reset();
+        myForm.querySelectorAll('[input="file"]').forEach((value) => {
+          const v = value as HTMLInputElement;
+          v.value = "";
+        });
+      })
+      .catch((e) => {
+        console.error(e);
+        toast.error("Form submission failed");
+      });
+  };
