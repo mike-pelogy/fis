feb33ec86b7f3718effe90d77a04be0b57db5629
Mike P <michael@pelogy.com>
2025-07-23 07:17:57 -0400

    fix: brif index

--- src/pages/etfs/brif/index.tsx
+++ src/pages/etfs/brif/index.tsx
@@ -106,7 +106,7 @@ const BrifPage: NextPageWithLayout<{
           <ETF
             etfData={etfData}
             handleDownloadHoldings={handleDownloadHoldings}
-            typeIndex={1}
+            typeIndex={0}
             overview={data.overview as Page_Brif_Overview}
             pricing={data.pricing as Page_Brif_Pricing}
             performance={data.performance as Page_Brif_Performance}
